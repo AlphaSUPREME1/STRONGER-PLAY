ac6ef4b1c2c6dfc7d5273e0dd663333cb0ce963b
AlphaSUPREME1 <a.k.mishra11010@gmail.com>
2026-01-12 14:43:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const base = "/STRONGER-PLAY";
+document.addEventListener("DOMContentLoaded", function () {
+  const base = "/STRONGER-PLAY";
 
-fetch(base + "/games.json")
-  .then(res => res.json())
-  .then(games => {
-    const container = document.getElementById("games-container");
-    container.innerHTML = "";
+  fetch(base + "/games.json")
+    .then(res => res.json())
+    .then(games => {
+      const container = document.getElementById("games-container");
+      container.innerHTML = "";
 
-    games.forEach(game => {
-      const div = document.createElement("div");
-      div.className = "game";
-      div.innerHTML = `
-        <a href="${base}/games/game.html?url=${encodeURIComponent(game.url)}">
-          <img src="${game.thumbnail}">
-          <p>${game.name}</p>
-        </a>
-      `;
-      container.appendChild(div);
+      games.forEach(game => {
+        const div = document.createElement("div");
+        div.className = "game";
+        div.innerHTML = `
+          <a href="${base}/games/game.html?url=${encodeURIComponent(game.url)}">
+            <img src="${game.thumbnail}">
+            <p>${game.name}</p>
+          </a>
+        `;
+        container.appendChild(div);
+      });
     });
-  });
+});
