c7dabc1efe809c6c28856b26a41b3c723244d312
AlphaSUPREME1 <a.k.mishra11010@gmail.com>
2026-01-12 14:41:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const base = "/STRONGER-PLAY";
 
 fetch(base + "/games.json")
-  .then(res => {
-    if (!res.ok) throw new Error("HTTP " + res.status);
-    return res.json();
-  })
+  .then(res => res.json())
   .then(games => {
     const container = document.getElementById("games-container");
     container.innerHTML = "";
@@ -12,19 +9,12 @@ fetch(base + "/games.json")
     games.forEach(game => {
       const div = document.createElement("div");
       div.className = "game";
-
       div.innerHTML = `
         <a href="${base}/games/game.html?url=${encodeURIComponent(game.url)}">
           <img src="${game.thumbnail}">
           <p>${game.name}</p>
         </a>
       `;
-
       container.appendChild(div);
     });
-  })
-  .catch(err => {
-    document.getElementById("games-container").innerHTML =
-      "<p style='color:red'>ERROR loading games.json</p>";
-    console.error(err);
   });
