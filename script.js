55be3c6cc898c27bcf8f2e3ca71ac6eb1fee0346
AlphaSUPREME1 <a.k.mishra11010@gmail.com>
2026-01-12 14:26:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-const base = "/STRONGER-PLAY";
+const repo = window.location.pathname.split("/")[1]; 
+const base = "/" + repo;
 
 fetch(base + "/games.json")
   .then(res => {
-    console.log("JSON status:", res.status);
+    if (!res.ok) throw new Error("HTTP " + res.status);
     return res.json();
   })
   .then(games => {
-    console.log("Games loaded:", games);
-
     const container = document.getElementById("games-container");
     container.innerHTML = "";
 
     games.forEach(game => {
       const div = document.createElement("div");
       div.className = "game";
+
       div.innerHTML = `
         <a href="${base}/games/game.html?url=${encodeURIComponent(game.url)}">
           <img src="${game.thumbnail}">
           <p>${game.name}</p>
         </a>
       `;
+
       container.appendChild(div);
     });
   })
   .catch(err => {
     document.getElementById("games-container").innerHTML =
-      "<p style='color:red'>ERROR loading games</p>";
-    console.error(err);
+      "<p style='color:red'>Cannot load games.json</p>";
+    console.error("Load error:", err);
   });
